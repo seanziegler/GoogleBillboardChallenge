feb0f0af5a4b9e69d30e36629c0833c7efc4532f
Sean Ziegler <sziegler0@gmail.com>
2019-07-30 16:29:02 -0400

    Updated fermatPrime to remove pow()

--- fermatPrime.py
+++ fermatPrime.py
@@ -1,17 +1,20 @@
 import math
+import random
 from fractions import gcd
 
 def fermatPrime(n):
-    tests = [1,5,7,12,33,54] # make random less than n
+    tests = [random.randint(2,10) for i in range(5)]
     prime = True
+
     for a in tests:
+        print(a,n)
         x = gcd(a,n) == 1 # why
-        y = (int(math.pow(a, n)) -1) % n == 1
+        y = (a**n - 1) % n == 1
         print('n: %s A: %s GCD: %s Fermat: %s' % (n, a, x, y))
         if not x or not y:
             prime = False
             break
     if prime:
-        print('%s is a prime number' % n)
+        print('%s is the first 10 digit prime in e.' % n)
 
     return prime
