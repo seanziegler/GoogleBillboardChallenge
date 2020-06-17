07165a40af53b17ec79a04d52a052b8c3ca57648
Sean Ziegler <sziegler0@gmail.com>
2020-06-16 21:26:39 -0400

    Adding readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Google Billboard Challenge
+
+Solving the Google billboard challenge using two methods: trial division and Fermat Theorem.
+
+### Completion
+
+![100%](https://progress-bar.dev/100)
+
+### Requirements
+
+### Completed Features
+
+- Solve using trial division
+- Solve using Fermat Theorem
+- Unit tests
+
+### License
+
+MIT (C) Sean Ziegler
--- fermatPrime.py
+++ fermatPrime.py
@@ -6,18 +6,17 @@ def fermatPrime(n):
         raise ValueError
     if not type(n) == int:
         raise TypeError
-    tests = [2,4,6,8,9]
+    tests = [2, 4, 6, 8, 9]
     prime = True
-
 
     for a in tests:
 
-        x = math.gcd(a,n) == 1 # why
+        x = math.gcd(a, n) == 1  # why
         y = pow(a, n-1, n) == 1
         if not x or not y:
             prime = False
             break
-        print(a,n,prime)
+        print(a, n, prime)
     if prime:
         print('%s is the first 10 digit prime in e.' % n)
 
